c775a2885a121f04518668c25b920f7137142b26
Andy Tran <undahvt@gmail.com>
2024-12-21 02:11:55 -0800

    header component

---
+++ weather-app/src/components/Header.tsx
@@ -0,0 +1,10 @@
+// src/components/Header.tsx
+import React from 'react';
+
+const Header: React.FC = () => (
+  <header style={{ padding: '1rem', textAlign: 'center', backgroundColor: '#f5f5f5' }}>
+    <h1>Outdoor Meetup Weather App</h1>
+  </header>
+);
+
+export default Header;
